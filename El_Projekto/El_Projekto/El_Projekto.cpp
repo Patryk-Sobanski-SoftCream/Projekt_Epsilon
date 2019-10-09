15e74913d5af06f2e1eb897884d1142a9784575b
Patryk Sobański <52403171+Patryk-Sobanski-SoftCream@users.noreply.github.com>
2019-10-09 22:43:29 +0200

    Add files via upload

---
+++ El_Projekto/El_Projekto/El_Projekto.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float x, epsilon, licznik, mianownik, wynik, wynik_ulamka, wynik2;
+	bool czy_dodac = false;
+
+	cout << "Podaj x: ";
+	cin >> x;
+	cout << "Podaj Epsilon: ";
+	cin >> epsilon;
+
+	licznik = 0.5;
+	mianownik = -1;
+	wynik = 0;
+
+	while (true) {
+		czy_dodac = !czy_dodac;
+		licznik *= x * x;
+		mianownik += 2;
+		wynik_ulamka = licznik / mianownik;
+		if (czy_dodac == false)
+		{
+			wynik2 = wynik - wynik_ulamka;
+		}
+		else
+		{
+			wynik2 = wynik + wynik_ulamka;
+		}
+		if (abs(wynik - wynik2) <= epsilon){
+			wynik = wynik2;
+			break;
+		}else{
+			wynik = wynik2;
+		}
+	}
+		cout << "Wynik: " << wynik;
+
+
+	return 0;
+}
